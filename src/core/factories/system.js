4f3822e37386f5faad69e957cfc9d5eeb0e721c1
bogdanz <bogdanz2015@gmail.com>
2018-08-25 22:13:59 +0300

    update systems factory

--- src/core/factories/system.js
+++ src/core/factories/system.js
@@ -1,4 +1,19 @@
-export const createSystem = onUpdateNode => (engine, componentTypes) => {
-  const node = engine.getNodeType(componentTypes)
-  engine.onUpdate(timestamp => node.each(onUpdateNode, timestamp))
+import { noop, isFunction } from '../util'
+
+export const createSystem = ({
+  onUpdateNode,
+  onStartUpdate = noop,
+  onEndUpdate = noop,
+}) => {
+  if (!isFunction(onUpdateNode)) {
+    throw new Error(`onUpdateNode must be a function`)
+  }
+  return (engine, componentTypes) => {
+    const node = engine.getNodeType(componentTypes)
+    engine.onUpdate(delta => {
+      onStartUpdate(delta)
+      node.each(item => onUpdateNode(item, delta))
+      onEndUpdate(delta)
+    })
+  }
 }
