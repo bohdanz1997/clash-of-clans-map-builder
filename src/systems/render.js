e441c8de23fef3d103ee7a14768c4fc9869d81ce
bogdanz <bogdanz2015@gmail.com>
2018-09-09 14:33:21 +0300

    refactor render system

--- src/systems/render.js
+++ src/systems/render.js
@@ -1,8 +1,8 @@
 import { createSystem } from '../core/factories'
 import { nRender } from '../nodes'
 
-export default $engine => createSystem({
-  update({ position, display }) {
-    display.sprite.position.copy(position.pos)
-  }
-})(nRender)($engine)
+const handler = ({ position, display }) => {
+  display.sprite.position.copy(position.pos)
+}
+
+export default $engine => createSystem(handler)(nRender)($engine)
