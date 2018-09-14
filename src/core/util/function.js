c68e9b2ffe6d21ce74f828c0ca1620bfb26de812
bogdanz <bogdanz2015@gmail.com>
2018-09-14 18:49:57 +0300

    add pipe function

--- src/core/util/function.js
+++ src/core/util/function.js
@@ -1,3 +1,4 @@
 export const identity = x => x
 export const noop = () => {}
 export const isFunction = func => typeof func === 'function'
+export const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
