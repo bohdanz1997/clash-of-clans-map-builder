86e8e9c5b7fc0aa847a006d33ca7e537e1e46bbc
bogdanz <bogdanz2015@gmail.com>
2018-10-09 00:29:13 +0300

    improve registerSystems, add default params handling

---
+++ src/core/systemPriorities.js
@@ -0,0 +1,3 @@
+export const PRE_INIT = 0
+export const PRE_UPDATE = 10
+export const POST_UPDATE = 20
--- src/registerSystems.js
+++ src/registerSystems.js
@@ -1,10 +1,43 @@
-import { jsModuleLoader } from './core'
+import { moduleLoader, jsFileNamesNormalizer } from './core'
 
-const systems = jsModuleLoader(require.context('./systems'))
+const defaultSystemParams = {
+  priority: -1,
+  enabled: true,
+}
+
+const buildSystemFromContext = (context) => {
+  const {
+    default: systemHandler,
+    params = {},
+  } = context
+
+  const systemParams = {
+    ...defaultSystemParams,
+    ...params,
+  }
+
+  return {
+    ...systemParams,
+    systemHandler,
+  }
+}
+
+const systemsLoader = moduleLoader(
+  jsFileNamesNormalizer,
+  buildSystemFromContext,
+)
+
+const systems = systemsLoader(require.context('./systems'))
+
 const sortByPriority = (a, b) => a.priority - b.priority
+const onlyEnabled = system => system.enabled
+const mapSystemHandler = system => system.systemHandler
 
 export default engine => (
   Object
     .values(systems)
+    .sort(sortByPriority)
+    .filter(onlyEnabled)
+    .map(mapSystemHandler)
     .map(engine.addSystem)
 )
