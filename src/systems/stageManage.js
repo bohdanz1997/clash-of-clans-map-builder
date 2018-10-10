40dc2c8bbdb1f27e353e37909c9bdf34057046ee
bogdanz <bogdanz2015@gmail.com>
2018-10-10 18:16:59 +0300

    update system stageManage for work with multiple layers

--- src/systems/stageManage.js
+++ src/systems/stageManage.js
@@ -1,12 +1,32 @@
-import { createSystem } from '../core/factories'
-import { nRender } from '../nodes'
+// @flow
+import type { GameConfig, Engine } from '../types/game'
+import type { Application } from '../types/pixi'
 
-export default ($engine, $app, $createLogger) => createSystem({
-  init(node) {
-    const logger = $createLogger('StageManageSystem')
-    const addSpriteToStage = ({ display }) => $app.stage.addChild(display.sprite)
-    node.each(addSpriteToStage)
+import { createLogger, systemPriorities } from '../core'
+import { Container } from '../core/pixi'
+import { createEnhancedSystem } from '../core/factories'
+import { nGroundLayer } from '../nodes'
 
-    logger.log('added', node.size, 'render items')
-  },
-})(nRender)($engine)
+export default ($config: GameConfig, $engine: Engine, $app: Application) => (
+  createEnhancedSystem({
+    init(groundLayerNode) {
+      const logger = createLogger('StageManageSystem')
+      const worldOffsetX = $config.hWidth - $config.hTileWidth
+      const groundLayer = new Container()
+
+      const addSpriteToStage = ({ display }) => {
+        groundLayer.addChild(display.sprite)
+      }
+
+      groundLayerNode.each(addSpriteToStage)
+      $app.stage.x += worldOffsetX
+      $app.stage.addChild(groundLayer)
+
+      logger.log('ground layer: added', groundLayerNode.size, 'render items')
+    },
+  })(nGroundLayer)($engine)
+)
+
+export const params = {
+  priority: systemPriorities.PRE_INIT,
+}
