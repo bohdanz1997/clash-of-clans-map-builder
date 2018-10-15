16409c4e81c6f62bb4dea664516c20bcfd229164
bogdanz <bogdanz2015@gmail.com>
2018-10-15 20:02:00 +0300

    use game-modules package

--- src/services/vendorUtils.js
+++ src/services/vendorUtils.js
@@ -1,9 +1,17 @@
+import { Bump, Tink, Smoothie, GameUtils, TileUtils, SpriteUtils } from 'game-modules'
 import { PIXI } from '../core/pixi'
 
-import GameUtils from '../vendor/gameUtils'
-import TileUtils from '../vendor/tileUtilities'
-import SpriteUtils from '../vendor/spriteUtilities'
-
+/** @type {GameUtilities} */
 export const gameUtils = new GameUtils()
+
+/** @type {TileUtilities} */
 export const tileUtils = new TileUtils(PIXI)
+
+/** @type {SpriteUtilities} */
 export const spriteUtils = new SpriteUtils(PIXI)
+
+/** @type {Bump} */
+export const bump = new Bump(PIXI)
+
+/** @type {Tink} */
+export const tink = new Tink(PIXI)
