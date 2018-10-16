99084a37446a27d99dd3887ee0bbf3c3f403ef04
bogdanz <bogdanz2015@gmail.com>
2018-10-16 11:22:27 +0300

    update pixi types

--- src/types/pixi.js
+++ src/types/pixi.js
@@ -4,6 +4,17 @@ export interface Point {
 
   set(x?: number, y?: number): void,
   copy(point: Point): void,
+  clone(): Point,
+  equals(p: Point): boolean,
+  add(point: Point): Point,
+  addNum(num: number): Point,
+  sub(point: Point): Point,
+  subNum(num: number): Point,
+  mult(point: Point): Point,
+  multNum(num: number): Point,
+  div(point: Point): Point,
+  divNum(num: number): Point,
+  floor(): Point,
 }
 
 export interface Rectangle {
@@ -33,11 +44,36 @@ export interface Container {
   y: number,
   name: string,
   position: Point,
-  addChild(any): any,
   children: Container[],
+
+  addChild(any): any,
   childByName(string): Container,
 }
 
 export interface Application {
   stage: Container,
+}
+
+type MatrixParams6 = [number, number, number, number, number, number]
+
+export interface Matrix {
+  IDENTITY: Matrix,
+  TEMP_MATRIX: Matrix,
+
+  fromArray([]): void,
+  set(...MatrixParams6): Matrix,
+  toArray(transpose: boolean, out?: []): [],
+  apply(pos: Point, newPos?: Point): Point,
+  applyInverse(pos: Point, newPos?: Point): Point,
+  translate(x: number, y: number): Matrix,
+  scale(x: number, y: number): Matrix,
+  rotate(angle: number): Matrix,
+  append(matrix: Matrix): Matrix,
+  setTransform(x: number, y: number, pivotX: number, pivotY: number, scaleX: number, scaleY: number, rotation: number, skewX: number, skewY: number): Matrix,
+  prepend(matrix: Matrix): Matrix,
+  decompose(transform: any): any,
+  invert(): Matrix,
+  identity(): Matrix,
+  clone(): Matrix,
+  copy(matrix: Matrix): Matrix,
 }
