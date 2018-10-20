b8ebeb701481af7c020d8b195ebdc5e5c7d3e558
bohdanz1997 <ladygaga16@>
2018-10-20 16:28:50 +0300

    update imports and refactor

--- src/systems/backgroundPrepare.js
+++ src/systems/backgroundPrepare.js
@@ -1,31 +1,25 @@
 // @flow
 import type { GameConfig, Engine } from '../types/game'
-import type { Application } from '../types/pixi'
 
 import { nBackGround } from '../nodes'
-
+import { Overlay } from '../entities'
 import { systemPriorities } from '../core'
 import { createEnhancedSystem } from '../core/factories'
 
-import { Overlay } from '../entities'
-
-export default ($config: GameConfig, $engine: Engine, $app: Application) => {
-
-  return createEnhancedSystem({
-    init(bgNode) {
-      // add overlay for each entity with backGround component
-      bgNode.each(({ display, position }) => {
-        const overlay = Overlay({
-          width: $config.hTileWidth,
-          height: $config.hTileWidth,
-          target: position,
-        })
-
-        $engine.addEntity(overlay)
+export default ($config: GameConfig, $engine: Engine) => createEnhancedSystem({
+  init(bgNode) {
+    // add overlay for each entity with backGround component
+    bgNode.each(({ display, position }) => {
+      const overlay = Overlay({
+        width: $config.hTileWidth,
+        height: $config.hTileWidth,
+        target: position,
       })
-    },
-  })(nBackGround)($engine)
-}
+
+      $engine.addEntity(overlay)
+    })
+  },
+})(nBackGround)($engine)
 
 export const params = {
   priority: systemPriorities.PRE_INIT,
