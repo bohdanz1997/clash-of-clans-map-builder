cfc500ace4ae050115a71ca254eb98c4363cb7cb
bohdanz1997 <ladygaga16@>
2018-10-24 23:33:59 +0300

    add elixirCollector

---
+++ src/entities/elixirCollector.js
@@ -0,0 +1,20 @@
+import * as c from '../components'
+import { withComponents, withIsoDisplay, pipeHOCs } from '../components/hoc'
+
+export default ({ id, x, y, offsetX, offsetY, isoWidth, isoHeight }, { $config }) => (
+  pipeHOCs(
+    withComponents(
+      c.GameObject(),
+      c.Building(),
+      c.Draggable(),
+      c.BackGround(),
+      c.Position({ x, y, offsetX, offsetY }),
+      c.IsoPosition(),
+      c.Collision({
+        width: $config.cartTileSize,
+        height: $config.cartTileSize,
+      }),
+    ),
+    withIsoDisplay(id),
+  )
+)
--- src/services/resourceLoader.js
+++ src/services/resourceLoader.js
@@ -3,11 +3,13 @@ import { loader } from 'core/pixi'
 import ground from 'assets/image/ground.png'
 import clanCastle from 'assets/image/clanCastle.png'
 import goldStorage from 'assets/image/goldStorage.png'
+import elixirCollector from 'assets/image/elixirCollector.png'
 
 export const resourceLoader = (done) => {
   loader
     .add('ground', ground)
     .add('clanCastle', clanCastle)
     .add('goldStorage', goldStorage)
+    .add('elixirCollector', elixirCollector)
     .load(done)
 }
