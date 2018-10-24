9984199145e3d2877a7cb79dd7830faf6ad298a0
bohdanz1997 <ladygaga16@>
2018-10-24 23:06:22 +0300

    add goldStorage

--- src/services/resourceLoader.js
+++ src/services/resourceLoader.js
@@ -1,13 +1,13 @@
 import { loader } from 'core/pixi'
 
-import * as atlas from 'assets/atlas/treasureHunter'
 import ground from 'assets/image/ground.png'
 import clanCastle from 'assets/image/clanCastle.png'
+import goldStorage from 'assets/image/goldStorage.png'
 
 export const resourceLoader = (done) => {
   loader
-    .add(atlas.file)
     .add('ground', ground)
     .add('clanCastle', clanCastle)
+    .add('goldStorage', goldStorage)
     .load(done)
 }
