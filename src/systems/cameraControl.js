4e122793c478668038658b9853432804c5e0a434
bohdanz1997 <ladygaga16@>
2018-10-25 07:32:09 +0300

    add smooth scale

--- src/systems/cameraControl.js
+++ src/systems/cameraControl.js
@@ -1,33 +1,62 @@
 // @flow
-import type { GameConfig, Engine } from 'types/game'
+import type { GameConfig, Engine, Keyboard } from 'types/game'
 
 import { createSystem } from 'core/factories'
+import { keys } from 'core/input'
+import type { Application } from 'types/pixi';
 import { nCameraControl } from '../nodes'
 import priorities from './priorities'
 
-export default ($config: GameConfig, $engine: Engine) => (
-  createSystem({
-    update({ camera, motion, control, position }) {
-      motion.vel.mult(motion.damp)
+const minScale = 0.75
+const maxScale = 1.5
+const scaleInc = 0.002
+const maxScaleInc = 0.1
+const damping = 0.9
 
-      camera.camera.x = position.pos.x
-      camera.camera.y = position.pos.y
+export default ($config: GameConfig, $engine: Engine, $keyboard: Keyboard, $app: Application) => {
+  $keyboard.addKeys(keys.PLUS, keys.MINUS)
+  const world = $app.stage.childByName('gameScene')
+  let inc = 0
 
-      if (control.up.isDown) {
-        motion.vel.y = -control.dy
-      }
-      if (control.down.isDown) {
-        motion.vel.y = control.dy
-      }
-      if (control.left.isDown) {
-        motion.vel.x = -control.dx
-      }
-      if (control.right.isDown) {
-        motion.vel.x = control.dx
-      }
-    },
-  })(nCameraControl)($engine)
-)
+  return (
+    createSystem({
+      update({ camera, motion, control, position }) {
+        motion.vel.mult(motion.damp)
+
+        camera.camera.x = position.pos.x
+        camera.camera.y = position.pos.y
+
+        if (control.up.isDown) {
+          motion.vel.y = -control.dy
+        }
+        if (control.down.isDown) {
+          motion.vel.y = control.dy
+        }
+        if (control.left.isDown) {
+          motion.vel.x = -control.dx
+        }
+        if (control.right.isDown) {
+          motion.vel.x = control.dx
+        }
+
+        if ($keyboard.isDown(keys.PLUS)) {
+          inc += scaleInc
+          inc = Math.min(inc, maxScaleInc)
+        } else if ($keyboard.isDown(keys.MINUS)) {
+          inc -= scaleInc
+          inc = Math.max(inc, -maxScaleInc)
+        }
+
+        let scale = world.scale.x + inc
+        if (scale > maxScale) scale = maxScale
+        else if (scale < minScale) scale = minScale
+
+        world.scale.set(scale)
+        inc *= damping
+      },
+    })(nCameraControl)($engine)
+  )
+}
 
 export const params = {
   priority: priorities.MOVEMENT,
