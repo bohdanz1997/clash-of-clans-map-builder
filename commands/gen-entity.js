fe73bcb20273a14608b8ac04c88418850474a6cc
SneakyRaven <sneaky-raven@gmail.com>
2018-12-19 18:05:09 +0200

    update gen entity command

--- commands/gen-entity.js
+++ commands/gen-entity.js
@@ -1,10 +1,8 @@
-import fs from 'fs'
 import program from 'commander'
 import { createLogger } from './log'
 
 import {
   pathGen,
-  strUtils,
   staticPaths as paths,
 } from './utils'
 
@@ -44,26 +42,9 @@ export default () => (
 )\n`
 }
 
-const indexFile = pathGen.entity('index')
 const entityFile = pathGen.entity(entity)
 
 genUtils.generate({
   filePath: entityFile,
   source: buildSource(entity, program.hoc),
 })
-
-const generateExportLine = name => `export { default as ${strUtils.capitalizeFirst(name)} } from './${name}'\n`
-
-const exportLine = generateExportLine(entity)
-const fileData = fs.readFileSync(indexFile, { encoding: 'utf8' })
-const lines = fileData.split('\n').filter(line => line !== '')
-const hasExportLine = lines.find(line => line.includes(entity))
-
-if (!hasExportLine) {
-  const finalLines = [...lines, exportLine]
-
-  genUtils.update({
-    filePath: indexFile,
-    source: finalLines.join('\n'),
-  })
-}
