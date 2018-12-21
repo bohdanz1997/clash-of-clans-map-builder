64282495d50eccc13a67ef1fc7eb43388f7fbc24
SneakyRaven <sneaky-raven@gmail.com>
2018-12-21 18:51:47 +0200

    feat(scent/system): add generic 'system' factory

--- core/scent/system.js
+++ core/scent/system.js
@@ -35,23 +35,39 @@ const baseCreateSystem = systemFactory => (handler, meta) => {
   return systemFactory(systemHandler)
 }
 
-const makeSystem = handler => componentTypes => (engine) => {
+const makeSystem = handler => nodeType => (engine) => {
   if (!handler.meta.enabled) return
-  const node = engine.getNodeType(componentTypes)
+  const nodes = engine.getNodeType(nodeType)
   engine.onUpdate((delta) => {
     handler.before(delta)
-    node.each(item => handler.update(item, delta))
+    nodes.each(node => handler.update(node, delta))
     handler.after(delta)
   })
-  handler.init(node)
+  handler.init(nodes)
 }
 
-const makeEnhancedSystem = handler => (...componentTypesList) => (engine) => {
+const makeEnhancedSystem = handler => (...arrayOfNodeTypes) => (engine) => {
   if (!handler.meta.enabled) return
-  const nodes = componentTypesList.map(engine.getNodeType)
-  engine.onUpdate(delta => handler.update(...nodes, delta, engine))
-  handler.init(...nodes)
+  const arrayOfNodes = arrayOfNodeTypes.map(engine.getNodeType)
+  engine.onUpdate(delta => handler.update(...arrayOfNodes, delta, engine))
+  handler.init(...arrayOfNodes)
 }
 
 export const createSystem = baseCreateSystem(makeSystem)
 export const createEnhancedSystem = baseCreateSystem(makeEnhancedSystem)
+
+export const system = (name, handler) => (...arrayOfNodeTypes) => {
+  if (!handler) {
+    // support handler as first param
+    handler = name
+  }
+
+  const nodeTypesAmount = arrayOfNodeTypes.length
+  if (nodeTypesAmount === 0) {
+    throw new Error('You must provide at least one nodeType to \'system\' factory')
+  }
+
+  return nodeTypesAmount === 1
+    ? createSystem(handler)(arrayOfNodeTypes[0])
+    : createEnhancedSystem(handler)(...arrayOfNodeTypes)
+}
