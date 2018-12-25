b8be555084430ae8f07ebf8c978fde0597d9ea4d
SneakyRaven <sneaky-raven@gmail.com>
2018-12-25 23:16:42 +0200

    chore(app/scenes): update GameScene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,67 +1,79 @@
-import {
-  asValue,
-} from 'awilix'
-import { createEntityBuilder } from 'core'
-import { Game } from 'core/boot'
-import { Scene } from 'core/scenes'
-import { TileMapParser } from 'core/tilemap'
+import { asFunction, asValue } from 'awilix'
 
-import Ignitor from '../Ignitor'
 import {
-  createEntityFactory,
-  createPositioning,
-} from '../services'
+  Scene,
+  EntityManager,
+  SystemManager,
+  TileMapParser,
+} from 'core'
+
+import { createPositioning } from '../services'
+import { priorities } from '../config/game'
+
+import * as s from '../systems'
+import * as entities from '../entities'
+
+const containerBuilder = container => (factory, data, dataForInject) => {
+  const injector = () => ({
+    data,
+    ...dataForInject,
+  })
+  const resolver = asFunction(factory).inject(injector)
+  return container.build(resolver)
+}
 
 export default class GameScene extends Scene {
-  /**
-   * @param {Game} game
-   */
-  constructor(game) {
+  constructor() {
     super('game')
 
-    this.game = game
+    /** @type {SystemManager} */
+    this.systemManager = null
+
+    /** @type {EntityManager} */
+    this.entityManager = null
   }
 
   preload() {
-    this.game.loader
-      .add('ground', 'assets/image/ground.png')
-      .add('clanCastle', 'assets/image/clanCastle.png')
-      .add('goldStorage', 'assets/image/goldStorage.png')
-      .add('elixirCollector', 'assets/image/elixirCollector.png')
+    this.loader
+      .add('ground', 'image/ground.png')
+      .add('clanCastle', 'image/clanCastle.png')
+      .add('goldStorage', 'image/goldStorage.png')
+      .add('elixirCollector', 'image/elixirCollector.png')
+      .add('myMap', 'map/first.json')
+      .add('entityDefinitions', 'entity/definitions.json')
   }
 
   create() {
-    const { container, engine, app, config } = this.game
+    this.systemManager = new SystemManager(
+      this.container,
+      this.engine,
+      { defaultPriority: priorities.UPDATE }
+    )
 
-    /** @type {Ignitor} */
-    const ignitor = container.resolve('ignitor')
+    const builder = containerBuilder(this.container)
+    this.entityManager = new EntityManager(entities, this.cache.get('entityDefinitions'), builder)
 
-    const systems = ignitor.resolveSystems()
-    const mapsDefinitions = ignitor.resolveMaps()
-    const entityFactories = ignitor.resolveEntities()
-    const entityDefinitions = ignitor.resolveEntityDefinitions()
+    const mapParser = new TileMapParser(this.entityManager.getAllDefinitions())
+    const map = mapParser.fromJSON(this.cache.get('myMap'))
+    const tileData = this.entityManager.getDefinition('tile')
 
-    const mapParser = new TileMapParser(entityDefinitions)
-    const map = mapParser.fromJSON(mapsDefinitions.first)
     map.createLayer('ground', {
-      objects: map.createEntitiesForLayer('tile', {}, entityDefinitions.tile),
+      objects: map.createEntitiesForLayer('tile', {}, tileData),
     })
 
-    const buildEntity = createEntityBuilder({
-      container,
-      entityFactories,
-    })
-
-    const entityFactory = createEntityFactory(buildEntity)
-
-    container.register({
+    this.container.register({
       map: asValue(map),
-      entityFactory: asValue(entityFactory),
-      world: asValue(app.stage.childByName('world')),
-      hud: asValue(app.stage.childByName('hud')),
-      positioning: asValue(createPositioning(config, app)),
+      entityFactory: asValue(this.entityManager),
+      world: asValue(this.app.stage.getChildByName('world')),
+      hud: asValue(this.app.stage.getChildByName('hud')),
+      positioning: asValue(createPositioning(this.config, this.app)),
     })
 
+    this.registerEntities(map)
+    this.registerSystems()
+  }
+
+  registerEntities(map) {
     const xy2World = (params) => {
       if (params.x !== undefined && params.y !== undefined) {
         params.x *= map.config.cellWidth
@@ -72,20 +84,38 @@ export default class GameScene extends Scene {
 
     map.layers.forEach((layer) => {
       layer.objects.forEach((entityData) => {
-        const entity = buildEntity(xy2World(entityData), {
-          mapConfig: map.config,
-        })
-        engine.addEntity(entity)
+        const entity = this.entityManager.create(
+          entityData.id,
+          xy2World(entityData),
+          { mapConfig: map.config },
+        )
+        this.engine.addEntity(entity)
       })
-    })
-
-    systems.forEach(({ systemHandler }) => {
-      const system = container.build(systemHandler)
-      engine.addSystem(() => system)
     })
   }
 
   registerSystems() {
-    
+    const { register, init } = this.systemManager
+
+    register(s.StagePrepare, priorities.PRE_INIT)
+    register(s.KeyboardUpdate, priorities.PRE_UPDATE)
+    register(s.Movement, priorities.MOVEMENT)
+    register(s.CameraControl, priorities.MOVEMENT)
+    register(s.CameraTouchControl, priorities.MOVEMENT)
+    register(s.CollisionUpdate, priorities.UPDATE_COLLISION)
+    register(s.BoundsLimiter, priorities.RESOLVE_COLLISIONS)
+    register(s.Debug)
+    register(s.DebugMapLayers)
+    register(s.DeckManager)
+    register(s.DragDrop)
+    register(s.GameMapUpdate)
+    register(s.InteractManager)
+    register(s.OverlayManager)
+    register(s.PointerManager)
+    register(s.IsoMovement, priorities.PRE_RENDER)
+    register(s.Render, priorities.RENDER)
+    register(s.IsoRender, priorities.ISO_RENDER)
+
+    init()
   }
 }
