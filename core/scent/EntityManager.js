556a4b15a88c67f076f070ccd0269c2132e5eb08
bogdanz <bogdanz2015@gmail.com>
2019-06-22 00:59:08 +0300

    feat(core/scent): add support function type

--- core/scent/EntityManager.js
+++ core/scent/EntityManager.js
@@ -1,5 +1,5 @@
 import { Game } from '../boot'
-import { objectReduce, firstToLower, noop } from '../util'
+import { objectReduce, firstToLower, noop, isFunction } from '../util'
 import { createEntity } from './entity'
 
 const getLevelData = (definition, level) => (
@@ -50,6 +50,10 @@ export class EntityManager {
   buildEntity(id, data = {}, dataForInject = {}) {
     if (!id) {
       throw new Error('Your must provide id param')
+    }
+
+    if (isFunction(id)) {
+      id = id.name.toLowerCase()
     }
 
     const Factory = this.getFactory(id)
