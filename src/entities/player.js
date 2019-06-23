be06ada11d2a07bbbc9ddf43d52e9800ebcc8cb6
bogdanz <bogdanz2015@gmail.com>
2019-06-23 09:52:50 +0300

    refactor(app): remove javascript-state-machine package

--- src/entities/player.js
+++ src/entities/player.js
@@ -1,22 +1,8 @@
-import StateMachine from 'javascript-state-machine'
 import { expolorer } from 'assets/atlas/treasureHunter'
 import { createEntity } from 'core/scent'
 import { DisplayFactory } from 'core/display'
 
 import * as c from '../components'
-
-const withFSM = (entity) => {
-  const fsm = new StateMachine({
-    init: 'idle',
-    transitions: [
-      { name: 'alerted', from: 'idle', to: 'alert' },
-      { name: 'attacked', from: 'alert', to: 'attack' },
-      { name: 'idled', from: 'attack', to: 'idle' },
-    ],
-  })
-
-  return entity.add(c.FSM({ fsm }))
-}
 
 export const Player = ({
   data: { x, y, width, height, speed, health, damage },
