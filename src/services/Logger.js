de578a7d63e18669c6e2c777b7979244b8e9bab8
bogdanz <bogdanz2015@gmail.com>
2019-06-25 11:13:36 +0300

    feat(app): add timestamp for logger

--- src/services/Logger.js
+++ src/services/Logger.js
@@ -1,17 +1,21 @@
+import dayjs from 'dayjs'
+
 export class Logger {
   constructor({ config }) {
     this.handler = config.debug ? console.log : () => {}
   }
 
   write(...args) {
-    this.handler(...args)
+    this.handler(new Date(), ...args)
   }
 }
+
+const now = () => dayjs().format('HH:mm:ss.ms')
 
 export const Log = ({ config }) => {
   const handler = config.debug ? console.log : () => {}
 
   return (...args) => {
-    handler(...args)
+    handler(`[${now()}]`, ...args)
   }
 }
